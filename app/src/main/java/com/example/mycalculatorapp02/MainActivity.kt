3f0d5d8a1512f3cc7715a9688b4f7b9e2f77a5b1
fsegurai <fabiansegu@gmail.com>
2024-06-10 08:43:22 -0600

    fix/operators actions - feat/percentage calculation

--- app/src/main/java/com/example/mycalculatorapp02/MainActivity.kt
+++ app/src/main/java/com/example/mycalculatorapp02/MainActivity.kt
@@ -26,6 +26,10 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * This function is called when a number button is clicked.
+     * @param view - The view that was clicked.
+     */
     fun numberAction(view: View) {
         if (view is Button) {
             if (view.text == ".") {
@@ -41,6 +45,10 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * This function is called when an operator button is clicked.
+     * @param view - The view that was clicked.
+     */
     fun operatorAction(view: View) {
         if (view is Button && canAddOperation) {
             binding.workingsTV.append(view.text);
@@ -49,11 +57,19 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * This function is called when the all clear button is clicked.
+     * @param view - The view that was clicked.
+     */
     fun allClearAction(view: View) {
         binding.workingsTV.text = "";
         binding.resultsTV.text = "";
     }
 
+    /**
+     * This function is called when the clear button is clicked.
+     * @param view - The view that was clicked.
+     */
     fun backSpaceAction(view: View) {
         val length = binding.workingsTV.text.length;
         if (length > 0) {
@@ -61,105 +77,127 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * This function is called when the equals button is clicked.
+     * @param view - The view that was clicked.
+     */
     fun equalsAction(view: View) {
-        binding.resultsTV.text = calculateResults();
+        binding.resultsTV.text = calculateResults()
     }
 
+    /**
+     * This function calculates the result of the expression in the workings text view.
+     */
     private fun calculateResults(): String {
-        val digitsOperators = digitsOperators();
+        val digitsOperators = digitsOperators()
+        if (digitsOperators.isEmpty()) return ""
 
-        if (digitsOperators.isEmpty()) return "";
+        val timesDivision = timesDivisionCalculate(digitsOperators)
+        if (timesDivision.isEmpty()) return ""
 
-        val timesDivision = timesDivisionCalculate(digitsOperators);
-
-        if (timesDivision.isEmpty()) return "";
-
-        val result = addSubstractCalculate(timesDivision);
-        return result.toString();
+        val result = addSubtractCalculate(timesDivision)
+        return result.toString()
     }
 
-    private fun addSubstractCalculate(passedList: MutableList<Any>): Float {
-        var result = passedList[0] as Float;
+    /**
+     * This function calculates the addition and subtraction of the expression.
+     * It takes a list of numbers and operators and returns the result.
+     * @param passedList - The list of numbers and operators.
+     */
+    private fun addSubtractCalculate(passedList: MutableList<Any>): Float {
+        var result = passedList[0] as Float
 
         for (i in passedList.indices) {
             if (passedList[i] is Char && i != passedList.lastIndex) {
                 val operator = passedList[i]
-                val nextDigit = passedList[i + 1] as Float;
-
-                when (operator) {
-                    '+' -> result += nextDigit;
-                    '-' -> result -= nextDigit;
-                }
+                val nextDigit = passedList[i + 1] as Float
+                if (operator == '+')
+                    result += nextDigit
+                if (operator == '-')
+                    result -= nextDigit
             }
         }
 
-        return result;
+        return result
     }
 
+    /**
+     * This function calculates the multiplication, division, and percentage of the expression.
+     * It takes a list of numbers and operators and returns the result.
+     * @param passedList - The list of numbers and operators.
+     */
     private fun timesDivisionCalculate(passedList: MutableList<Any>): MutableList<Any> {
-        var list = passedList;
-
-        while (list.contains("x") || list.contains("/")) {
-            list = calcTimesDiv(list);
+        var list = passedList
+        while (list.contains('x') || list.contains('/') || list.contains('%')) {
+            list = calcTimesDiv(list)
         }
-
-        return list;
+        return list
     }
 
+    /**
+     * This function calculates the multiplication, division, and percentage of the expression.
+     * It takes a list of numbers and operators and returns the result.
+     * @param passedList - The list of numbers and operators.
+     */
     private fun calcTimesDiv(passedList: MutableList<Any>): MutableList<Any> {
-        val newList = mutableListOf<Any>();
-        var restartIndex = passedList.size;
+        val newList = mutableListOf<Any>()
+        var restartIndex = passedList.size
 
         for (i in passedList.indices) {
             if (passedList[i] is Char && i != passedList.lastIndex && i < restartIndex) {
-                val operator = passedList[i];
-                val prevDigit = passedList[i - 1] as Float;
-                val nextDigit = passedList[i + 1] as Float;
-
+                val operator = passedList[i]
+                val prevDigit = passedList[i - 1] as Float
+                val nextDigit = passedList[i + 1] as Float
                 when (operator) {
                     'x' -> {
-                        newList.add(prevDigit * nextDigit);
-                        restartIndex = i + 1;
+                        newList.add(prevDigit * nextDigit)
+                        restartIndex = i + 1
                     }
 
                     '/' -> {
-                        newList.add(prevDigit / nextDigit);
-                        restartIndex = i + 1;
+                        newList.add(prevDigit / nextDigit)
+                        restartIndex = i + 1
+                    }
+
+                    '%' -> {
+                        newList.add(prevDigit * nextDigit / 100)
+                        restartIndex = i + 1
                     }
 
                     else -> {
-                        newList.add(prevDigit);
-                        newList.add(operator);
+                        newList.add(prevDigit)
+                        newList.add(operator)
                     }
                 }
             }
 
-            if (i > restartIndex) {
-                newList.add(passedList[i]);
-            }
+            if (i > restartIndex)
+                newList.add(passedList[i])
         }
 
-        return newList;
+        return newList
     }
 
+    /**
+     * This function converts the workings text view into a list of numbers and operators.
+     * It returns the list.
+     */
     private fun digitsOperators(): MutableList<Any> {
-        val list = mutableListOf<Any>();
-        var currentDigit = "";
-
+        val list = mutableListOf<Any>()
+        var currentDigit = ""
         for (character in binding.workingsTV.text) {
-            if (character.isDigit() || character == '.') {
-                currentDigit += character;
-            } else {
-                list.add(currentDigit);
-                list.add(character);
-                currentDigit = "";
+            if (character.isDigit() || character == '.')
+                currentDigit += character
+            else {
+                list.add(currentDigit.toFloat())
+                currentDigit = ""
+                list.add(character)
             }
         }
 
-        if (currentDigit != "") {
-            list.add(currentDigit.toFloat());
-        }
+        if (currentDigit != "")
+            list.add(currentDigit.toFloat())
 
-        return list;
+        return list
     }
 }
